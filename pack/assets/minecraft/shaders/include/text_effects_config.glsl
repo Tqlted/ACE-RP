f02f906c36d3c41137d49791d528d85a64395d31
RedVortexDev <58099979+RedVortexDev@users.noreply.github.com>
2024-10-28 17:22:07 +0200

    dev text

--- pack/assets/minecraft/shaders/include/text_effects_config.glsl
+++ pack/assets/minecraft/shaders/include/text_effects_config.glsl
@@ -6,19 +6,19 @@
 // UTIL 
 
     // WHITE + NO BACKGROUND
-    TEXT_EFFECT(240, 240, 4) {
+    TEXT_EFFECT(240, 240, 4) { // #F0F004
         override_text_color(rgb(255,255,255));
         remove_text_shadow();
     }
 
     // WHITE + BLACK BACKGROUND
-    TEXT_EFFECT(240, 240, 8) {
+    TEXT_EFFECT(240, 240, 8) { // #F0F008
         override_shadow_color(rgb(0,0,0));
     }
 
 // BOSSBARS 
     // LOBBY 2ND BOSSBAR
-    TEXT_EFFECT(240, 240, 12) {
+    TEXT_EFFECT(240, 240, 12) { // #F0F00C
         apply_iterating_movement(1, 2);
         change_position(0,-22);
         apply_gradient(rgb(238, 102, 102),rgb(208, 72, 72));
@@ -26,27 +26,33 @@
     }
 
     // BUY 2ND BOSSBAR
-    TEXT_EFFECT(240, 240, 24) {
+    TEXT_EFFECT(240, 240, 24) { // #F0F018
         change_position(0,-22);
         apply_metalic(rgb(249, 222, 87),rgb(233, 164, 21));
         apply_outline(rgb(102, 31, 13));
     }
 
-// DEV
-TEXT_EFFECT(240, 240, 16) {
+// DEV LORE
+TEXT_EFFECT(240, 240, 16) { // #F0F010
     apply_gradient(rgb(170, 85, 255),rgb(200, 115, 255));
     override_shadow_color(rgb(0,0,0));
 }
 
+// DEV NAME
+TEXT_EFFECT(240, 240, 28) { // #F0F01C
+    override_text_color(rgb(255,255,255));
+    apply_outline(rgb(150, 65, 235));
+}
+
 // CARDS
-TEXT_EFFECT(240, 240, 20) {
+TEXT_EFFECT(240, 240, 20) { // #F0F014
     override_text_color(rgb(255,255,255));
     remove_text_shadow();
     apply_thin_outline(rgb(0,0,0));
 }
 
 // VOTE TEXT
-TEXT_EFFECT(240, 240, 236) {
+TEXT_EFFECT(240, 240, 236) { // #F0F0EC
     apply_rainbow();
 }
 
